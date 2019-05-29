13f709928123827359c5f33415b6650403ff3544
Moustafa Alzantot <malzantot@ucla.edu>
2019-05-29 11:57:49 -0700

    3 layers decoder

--- rvae_model.py
+++ rvae_model.py
@@ -72,7 +72,11 @@ class RNNDecoder(tf.keras.Model):
         self.rnn_units = rnn_units
         self.num_feats = num_feats
         self.num_labels = num_labels
-        self.gru = layers.CuDNNGRU(
+        self.gru1 = layers.CuDNNGRU(
+            self.rnn_units, return_state=True, return_sequences=True)
+        self.gru2 = layers.CuDNNGRU(
+            self.rnn_units, return_state=True, return_sequences=True)
+        self.gru3 = layers.CuDNNGRU(
             self.rnn_units, return_state=True, return_sequences=True)
         self.fc_out = layers.Dense(self.num_feats)
         self.label_emb = layers.Embedding(self.num_labels, 6)
@@ -83,37 +87,44 @@ class RNNDecoder(tf.keras.Model):
         y_emb = tf.tile(tf.expand_dims(y_emb, axis=1), [1, time_len, 1])
 
         rnn_input = tf.concat([x, y_emb], axis=-1)
-        output, last_state = self.gru(rnn_input, hidden)
-        output = self.fc_out(output)
-        return output, last_state
+        output1, last_state1 = self.gru1(rnn_input, hidden[0])
+        output2, last_state2 = self.gru2(output1, hidden[1])
+        output3, last_state3 = self.gru3(output2, hidden[2])
+
+        output = self.fc_out(output3)
+        new_hidden = [last_state1, last_state2, last_state3]
+        return output, new_hidden
 
 
 class RVAEModel(tf.keras.Model):
 
-    def __init__(self, enc_rnn_units=64, z_dim=64, dec_rnn_units=64, num_feats=6, num_labels=6):
+    def __init__(self, enc_rnn_units=64, z_dim=64, dec_rnn_units=64, num_feats=6, num_labels=6, z_input=True):
         super(RVAEModel, self).__init__()
         self.enc_rnn_units = enc_rnn_units
         self.z_dim = z_dim
         self.dec_rnn_units = dec_rnn_units
         self.num_feats = num_feats
         self.num_labels = num_labels
+        self.z_input = z_input
         self.encoder = RNNEncoder(enc_rnn_units)
         self.decoder = RNNDecoder(dec_rnn_units, num_feats, num_labels)
         self.fc_z = layers.Dense(6)
+        self.fc_hidden = layers.Dense(3*self.dec_rnn_units)
 
     def __call__(self, x, y):
         batch_size, time_len, feat_dim = x.shape
         z, mu, log_var = self.encoder(x)
 
         decoder_outputs = []
-        dec_init_state = z  # initialize the decoder state with the z vector
+        # initialize the decoder state with the z vector
+        dec_init_state = tf.reshape(self.fc_hidden(z), [3, batch_size, -1])
         init_step = tf.zeros_like(x[:, 0:1, :])
         dec_input = tf.concat([init_step, x[:, :-1]], axis=1)
-        z_emb = self.fc_z(z)
-        z_with_time = tf.tile(tf.expand_dims(
-            z_emb, 1), [1, time_len, 1])
-
-        dec_input = tf.concat([z_with_time, dec_input], axis=2)
+        if self.z_input:
+            z_emb = self.fc_z(z)
+            z_with_time = tf.tile(tf.expand_dims(
+                z_emb, 1), [1, time_len, 1])
+            dec_input = tf.concat([z_with_time, dec_input], axis=2)
         recon_output, _ = self.decoder(dec_input, dec_init_state, y)
         return recon_output, mu, log_var
 
@@ -123,11 +134,14 @@ class RVAEModel(tf.keras.Model):
         z = tf.random_normal(shape=(num_examples, self.decoder.rnn_units))
         last_pred = tf.zeros(shape=(num_examples, 1, self.num_feats))
         preds = []
-        last_state = z
+        last_state = tf.reshape(self.fc_hidden(z), [3, num_examples, -1])
         z_emb = self.fc_z(z)
         z_with_time = tf.expand_dims(z_emb, axis=1)
         for _ in range(max_len):
-            step_input = tf.concat([z_with_time, last_pred], axis=2)
+            if self.z_input:
+                step_input = tf.concat([z_with_time, last_pred], axis=2)
+            else:
+                step_input = last_pred
             last_pred, last_state = self.decoder(
                 step_input, last_state, labels)
             preds.append(last_pred)
