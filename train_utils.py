a207c121876c157cba97da82a734e6a28f00bb8c
Moustafa Alzantot <malzantot@ucla.edu>
2019-07-07 17:47:20 -0700

    tuning the weights (recon_loss = 50, adv_loss=25)

--- train_utils.py
+++ train_utils.py
@@ -227,8 +227,8 @@ def rvae_adv_train_epoch(g_model, d_model, train_data, g_optim, d_optim, epoch_i
 
             g_recon_loss = recon_loss
             print('\t', recon_loss, ' ', z_loss)
-            g_loss = (ratio + 0.25) * (100*g_recon_loss) + \
-                (1-ratio)*(10 * z_loss + 50*g_adv_loss +
+            g_loss = (ratio + 0.25) * (50*g_recon_loss) + \
+                (1-ratio)*(10 * z_loss + 25*g_adv_loss +
                            tf.maximum(kl_loss, 0.10/batch_size))
 
         print('\t', d_loss.numpy(), ' ; ', g_loss.numpy())
