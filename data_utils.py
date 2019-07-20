89f3aafdbdea4b127de7a94515a10d4a100058fe
Moustafa Alzantot <malzantot@ucla.edu>
2019-07-19 18:18:45 -0700

    nit: change dataset names

--- data_utils.py
+++ data_utils.py
@@ -21,29 +21,44 @@ class DataFactory:
         if dset_name == 'har':
             dset_root = 'dataset/har'
             dset_class = HARDataset
+            mini = True
         elif dset_name == 'adl':
             dset_root = 'dataset/adl'
             dset_class = ADLDataset
+            mini = True
+        elif dset_name == 'adl_full':
+            dset_root = 'dataset/adl'
+            dset_class = ADLDataset
+            mini = False 
         elif dset_name == 'cinc':
             dset_root = 'dataset/cinc'
             dset_class = CINCDataset
+            mini = False
         elif dset_name == 'ecg2lead':
             dset_root = 'dataset/ecg2lead'
             dset_class = ECG2LeadDataset
+            mini = False
         elif dset_name == 'ecg200':
             dset_root = 'dataset/ecg200'
             dset_class = ECG200Dataset
+            mini = False
         elif dset_name == 'ecg':
             dset_root = 'dataset/ecg_data'
             dset_class = ECGDataset
+            mini = True
+        elif dset_name == 'ecg_full':
+            dset_root = 'dataset/ecg_data'
+            dset_class = ECGDataset
+            mini = False          
         elif dset_name == 'dummy':
             dset_root = None
             dset_class = DummyDataset
+            mini = False
         else:
             raise Exception("Invalid dataset requested", dset_name)
 
-        train_data = dset_class(dset_root, is_train=True, mini=True)
-        test_data = dset_class(dset_root, is_train=False, mini=True)
+        train_data = dset_class(dset_root, is_train=True, mini=mini)
+        test_data = dset_class(dset_root, is_train=False, mini=mini)
 
         dset_meta = Metadata(train_data.num_feats,
                              train_data.num_labels, train_data.classes, train_data.max_len, train_data.labels.shape[0])
